2f7d6f14f3ef5071c27ee8dc22ea968d225ff12f
islamdev2022 <islam.birouk.2004@gmail.com>
2024-08-17 01:03:06 +0100

    make any text with '' bold

---
+++ Quiz-app/src/Components/BoldText.jsx
@@ -0,0 +1,21 @@
+const BoldText = ({ text,className }) => {
+    if (!text) return null;
+  
+    const parts = text.split(/('.*?'|<.*?>|“.*?”|‘.*?’)/);  
+    return (
+      <p className={`text-gray-700 ${className}`}>
+        {parts.map((part, index) =>
+           part.match(/'.*?'|<.*?>|“.*?”|‘.*?’/) ? (
+            // If the part is inside single quotes, make it bold
+            <span key={index} className="font-bold">
+              {part}
+            </span>
+          ) : (
+            <span key={index}>{part}</span>
+          )
+        )}
+      </p>
+    );
+  };
+
+export default BoldText;
